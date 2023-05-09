03b2cdea6404e7fd0982677b43c0f63a1eb09a5b
Maks <mpasiecki@gmail.com>
2023-05-09 15:30:16 +0100

    implement API endpoint to retrieve topics data

---
+++ models/topics.models.js
@@ -0,0 +1,12 @@
+const db = require("./../db/connection.js");
+exports.fetchTopics = () => {
+  return db
+    .query(
+      `
+    SELECT * FROM topics;
+    `
+    )
+    .then((result) => {
+      return result.rows;
+    });
+};
