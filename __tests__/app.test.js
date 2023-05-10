8164dcdf0b2fd5eff79cbfeb5542afc2ace27e30
Maks <mpasiecki@gmail.com>
2023-05-10 11:27:37 +0100

    implement API endpoint to retrieve article by id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -45,3 +45,44 @@ describe("/api", () => {
       });
   });
 });
+
+describe("/api/articles/:article_id", () => {
+  it("GET status:200, responds with an article object by ID provided", () => {
+    return request(app)
+      .get("/api/articles/3")
+      .expect(200)
+      .then((res) => {
+        const articleObject = res.body.article;
+        const expected = {
+          article_id: 3,
+          title: "Eight pug gifs that remind me of mitch",
+          topic: "mitch",
+          author: "icellusedkars",
+          body: "some gifs",
+          created_at: "2020-11-03T09:12:00.000Z",
+          votes: 0,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        };
+        expect(articleObject).toEqual(expected);
+      });
+  });
+  it("GET status: 400, returns error message when received invalid id", () => {
+    return request(app)
+      .get("/api/articles/article_3")
+      .expect(400)
+      .then((res) => {
+        const responseMessage = res.body.message;
+        expect(responseMessage).toBe("Bad request.");
+      });
+  });
+  it("GET status: 404, returns error message when received unavailable id", () => {
+    return request(app)
+      .get("/api/articles/998")
+      .expect(404)
+      .then((res) => {
+        const responseMessage = res.body.message;
+        expect(responseMessage).toBe("No articles has been found.");
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const { getTopics } = require("./controllers/topics.controllers.js");
 const { errorHandler } = require("./errors/errors.js");
+const { getTopics } = require("./controllers/topics.controllers.js");
 const { getAllEndpoints } = require("./controllers/endpoints.controllers.js");
+const { getArticleById } = require("./controllers/articles.controllers.js");
 
 app = express();
 
 app.get("/api/topics", getTopics);
 
 app.get("/api", getAllEndpoints);
+
+app.get("/api/articles/:article_id", getArticleById);
 
 app.use(errorHandler);
 
